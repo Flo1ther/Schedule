5a5b912840b0260005562025ab70bd4305860f6b
Flo1ther <shevchuk.nirosla@gmail.com>
2024-10-05 13:49:04 +0300

    Добавьте файлы проекта.

---
+++ Schedule/Program.cs
@@ -0,0 +1,50 @@
+﻿using Schedule;
+
+ScheduleForClass mathClass = new ScheduleForClass();
+mathClass.StartTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 16, 30, 0);
+mathClass.EndTime = mathClass.StartTime.AddMinutes(80);
+mathClass.Subject = "Mathematics";
+mathClass.Teacher = "Mr. Johnson";
+mathClass.Classroom = "Room 101";
+
+ScheduleForClass physicsClass = new ScheduleForClass();
+physicsClass.StartTime = mathClass.EndTime.AddMinutes(10);
+physicsClass.EndTime = physicsClass.StartTime.AddMinutes(80);
+physicsClass.Subject = "Physics";
+physicsClass.Teacher = "Dr. Smith";
+physicsClass.Classroom = "Room 102";
+
+Console.WriteLine("Schedule:");
+
+Console.WriteLine("** Math Class **");
+Console.WriteLine($"Start Time: {mathClass.StartTime}");
+Console.WriteLine($"End Time: {mathClass.EndTime}");
+Console.WriteLine($"Subject: {mathClass.Subject}");
+Console.WriteLine($"Teacher: {mathClass.Teacher}");
+Console.WriteLine($"Classroom: {mathClass.Classroom}");
+Console.WriteLine();
+Console.WriteLine("---------- Break (10 minutes) ----------");
+
+Console.WriteLine("** Physics Class **");
+Console.WriteLine($"Start Time: {physicsClass.StartTime}");
+Console.WriteLine($"End Time: {physicsClass.EndTime}");
+Console.WriteLine($"Subject: {physicsClass.Subject}");
+Console.WriteLine($"Teacher: {physicsClass.Teacher}");
+Console.WriteLine($"Classroom: {physicsClass.Classroom}");
+
+bool isLateForMath = ScheduleForClass.IsLate(mathClass.StartTime);
+
+if (isLateForMath)
+{
+    Console.WriteLine("You are late for the Math class.");
+}
+else
+{
+    Console.WriteLine("You aren't late for the Math class.");
+}
+
+mathClass.ChangeDuration(90);
+
+ScheduleForClass.SwapClasses(mathClass, physicsClass);
+
+physicsClass.CancelClass();
---
+++ Schedule/Schedule.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule
+{
+    internal class ScheduleForClass
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; }
+        public string Subject { get; set; }
+        public string Teacher { get; set; }
+        public string Classroom { get; set; }
+
+        public static bool IsLate(DateTime startTime)
+        {
+            Console.WriteLine("What time did you arrive? (Format: HH:MM)");
+            var arrivalTime = DateTime.Parse(Console.ReadLine());
+
+            return arrivalTime > startTime;
+        }
+
+        public void ChangeDuration(int newDuration)
+        {
+            this.EndTime = this.StartTime.AddMinutes(newDuration);
+            Console.WriteLine($"The duration of '{this.Subject}' has been changed to {newDuration} minutes.");
+        }
+
+        public void CancelClass()
+        {
+            this.StartTime = DateTime.MinValue;
+            this.EndTime = DateTime.MinValue;
+            Console.WriteLine($"The class '{this.Subject}' has been canceled.");
+        }
+
+        public static void SwapClasses(ScheduleForClass firstClass, ScheduleForClass secondClass)
+        {
+            var tempStartTime = firstClass.StartTime;
+            var tempEndTime = firstClass.EndTime;
+            var tempSubject = firstClass.Subject;
+            var tempTeacher = firstClass.Teacher;
+            var tempClassroom = firstClass.Classroom;
+
+            firstClass.StartTime = secondClass.StartTime;
+            firstClass.EndTime = secondClass.EndTime;
+            firstClass.Subject = secondClass.Subject;
+            firstClass.Teacher = secondClass.Teacher;
+            firstClass.Classroom = secondClass.Classroom;
+
+            secondClass.StartTime = tempStartTime;
+            secondClass.EndTime = tempEndTime;
+            secondClass.Subject = tempSubject;
+            secondClass.Teacher = tempTeacher;
+            secondClass.Classroom = tempClassroom;
+
+            Console.WriteLine("The classes have been swapped.");
+        }
+    }
+}
